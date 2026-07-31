6e8d13c241fb4993254b0e484ac7638417a7bbf9
felix <felix@quanzhan.cc>
2026-07-31 11:24:17 +0800

    Prepare OmniDock 1.2.1

--- script/build_and_run.sh
+++ script/build_and_run.sh
@@ -5,8 +5,8 @@ MODE="${1:-run}"
 APP_NAME="OmniDock"
 BUNDLE_ID="com.quanzhankeji.OmniDock"
 MIN_SYSTEM_VERSION="12.3"
-MARKETING_VERSION="1.2.0"
-BUILD_NUMBER="10"
+MARKETING_VERSION="1.2.1"
+BUILD_NUMBER="11"
 
 usage() {
   echo "usage: $0 [run|--stage|--debug|--logs|--telemetry|--verify|--install|--install-finder-extension]" >&2
